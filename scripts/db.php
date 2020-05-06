647d6fb9c6fb31a516527ea3dc53c4a930b796b0
Tiwonge Banda <47220802+tiwobanda@users.noreply.github.com>
2020-05-06 23:55:24 +0100

    Update db.php

--- scripts/db.php
+++ scripts/db.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', ''); //remember to place the db username
+define('DB_PASSWORD', ''); //remember to place the db password
 define('DB_DATABASE', 'resshare_db');
 
 $dbcon = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
